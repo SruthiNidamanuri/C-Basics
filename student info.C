80bad210d07fba879b1af09e36471a37374babf4
SruthiNidamanuri <92291916+SruthiNidamanuri@users.noreply.github.com>
2021-10-20 12:31:08 +0530

    Add files via upload

---
+++ student info.C
@@ -0,0 +1,16 @@
+/*c program to print Hello World! */
+#include<stdio.h>
+int main()
+{
+	// variable Declaration
+	int age;
+	float cgpa;
+	char initial;
+	
+	// Initialization
+	age = 21;
+	cgpa = 7.6;
+	initial = 'M';
+	 
+	 // Output
+}
